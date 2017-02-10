a16021fe8e6199dd801a06764a9bdd4e311191f0
Gaubee <gaubeebangeel@gmail.com>
2017-02-11 00:00:47 +0800

    移除fillMode
    Signed-off-by: Gaubee <gaubeebangeel@gmail.com>

--- src/modules/PIXI/Image.js
+++ src/modules/PIXI/Image.js
@@ -1,96 +1,3 @@
-const FILLMODE_IMAGE_DRAW_CALC = [
-	function() { // 0 
-		const {
-			width,
-			height
-		} = this;
-		return {
-			width,
-			height,
-			x: 0,
-			y: 0
-		}
-	},
-	[ // 1
-		function() {
-			const {
-				width,
-				height,
-				$src_ratio
-			} = this;
-			const res_height = width / $src_ratio;
-			return {
-				x: 0,
-				y: (height - res_height) / 2,
-				width: width,
-				height: res_height,
-			}
-		},
-		function() {
-			const {
-				width,
-				height,
-				$src_ratio
-			} = this;
-			const res_width = height * $src_ratio;
-			return {
-				x: (width - res_width) / 2,
-				y: 0,
-				width: res_width,
-				height: height,
-			}
-		}
-	],
-	[ // 2
-		function() {
-			const {
-				width,
-				height,
-				$cur_ratio,
-			} = this;
-
-			const texture = this.$sprite.texture;
-			const frame = texture.frame;
-			const _bak_frame = texture._bak_frame || (texture._bak_frame = frame.clone());
-			const frame_width = _bak_frame.width;
-			const frame_height = _bak_frame.height;
-			const res_frame_width = frame_height * $cur_ratio;
-			frame.width = res_frame_width;
-			frame.x = (frame_width - res_frame_width) / 2;
-			texture._updateUvs();
-			return {
-				x: 0,
-				y: 0,
-				width: width,
-				height: height,
-			}
-		},
-		function() {
-			const {
-				width,
-				height,
-				$cur_ratio,
-			} = this;
-
-			const texture = this.$sprite.texture;
-			const frame = texture.frame;
-			const bak_frame = texture._bak_frame || (texture._bak_frame = frame.clone());
-			const frame_width = bak_frame.width;
-			const frame_height = bak_frame.height;
-			const res_frame_height = frame_width / $cur_ratio;
-			frame.height = res_frame_height;
-			frame.y = (frame_height - res_frame_height) / 2;
-			texture._updateUvs();
-			return {
-				x: 0,
-				y: 0,
-				width: width,
-				height: height,
-			}
-		}
-	]
-];
-
 class Image extends Container {
 	constructor(meta) {
 		super(meta);
@@ -99,7 +6,6 @@ class Image extends Container {
 			progress: "real",
 			src: "url",
 			color: "color",
-			fillMode: "enum",
 		});
 		/** repeat
 		 * Array
@@ -253,9 +159,6 @@ class Image extends Container {
 		this.srcChanged.connect(this, this.$onSourceChanged);
 		this.widthChanged.connect(this, this.$reDrawWH);
 		this.heightChanged.connect(this, this.$reDrawWH);
-		this.fillModeChanged.connect(this, this.$onFillModeChanged);
-		this.$getImgWH = FILLMODE_IMAGE_DRAW_CALC[0];
-
 	}
 	abort() {
 		const $request = this.$request;
@@ -314,8 +217,6 @@ class Image extends Container {
 	$afterSourceChanged(texture) {
 		this.$sprite.setTexture(texture);
 		this.$onHtmlImageLoad(0, 1); // reset width height 
-		this.$getImgWH = null; // force reDraw
-		this.$onFillModeChanged(); // reset $getImgWH and reDraw 
 	}
 	$onHtmlImageLoad(e, prevent_draw) {
 		const $htmlImage = this.$htmlImage;
@@ -359,13 +260,8 @@ class Image extends Container {
 		const $sprite = this.$sprite;
 		const container = this.dom;
 		const {
-			x,
-			y,
 			width,
 			height,
-		} = this.$getImgWH()
-		$sprite.position.set(x, y);
-		const {
 			$repeat_x,
 			$repeat_y
 		} = this;
@@ -400,49 +296,6 @@ class Image extends Container {
 	}
 	get $cur_ratio() {
 		return Math.abs(this.width / this.height);
-	}
-	$onFillModeChanged(newVal, oldVal, name) {
-		const $sprite = this.$sprite;
-		const texture = $sprite.texture;
-		if (name === "fillMode") {
-			if (oldVal === 2) { // PreserveAspectCrop
-				const frame = texture.frame;
-				const _bak_frame = texture._bak_frame;
-				if (_bak_frame) {
-					frame.copy(_bak_frame);
-					texture._updateUvs();
-				}
-			}
-		}
-		const currentFillMode = this.fillMode;
-		let $getImgWH;
-		switch (currentFillMode) {
-			case 0:
-				$getImgWH = FILLMODE_IMAGE_DRAW_CALC[0]
-				break;
-			case 1: // PreserveAspectFit
-			case 2: // PreserveAspectCrop
-				if (this.$src_ratio > this.$cur_ratio) {
-					$getImgWH = FILLMODE_IMAGE_DRAW_CALC[currentFillMode][0]
-				} else {
-					$getImgWH = FILLMODE_IMAGE_DRAW_CALC[currentFillMode][1]
-				}
-				break;
-			case 3:
-				break;
-			case 4:
-				break;
-			case 5:
-				break;
-			case 6:
-				break;
-			default:
-				throw new TypeError("Error fillMode:", currentFillMode)
-		}
-		if ($getImgWH && this.$getImgWH !== $getImgWH) {
-			this.$getImgWH = $getImgWH;
-			this.$reDrawWH();
-		}
 	}
 }
 
