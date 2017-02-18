db763af21430ef83b8d56aa911f25ee9b5f5675c
Gaubee <gaubeebangeel@gmail.com>
2017-02-18 19:36:48 +0800

    增加绑定接口，可动态实现属性之间的绑定 eg:img1.setBinding('anchors.left','mrect1.left');
    Signed-off-by: Gaubee <gaubeebangeel@gmail.com>

--- src/modules/PIXI/0_Item.js
+++ src/modules/PIXI/0_Item.js
@@ -48,6 +48,26 @@ class PixiObject extends QmlWeb.QObject {
     component.childrenRect.width = maxWidth;
     component.childrenRect.height = maxHeight;
   }
+  setBinding(key, code) {
+    let keys;
+    if (typeof key === "string") {
+      keys = key.split(".")
+    } else if (key instanceof Array) {
+      keys = key;
+    } else {
+      return
+    }
+
+    let ctx = this;
+    keys.forEach((key, i) => {
+      if (i < keys.length - 1) {
+        ctx = ctx[key]
+      } else {
+        ctx.$properties[key].set(new QmlWeb.QMLBinding(code),
+          QmlWeb.QMLProperty.ReasonUser, this, this.$context);
+      }
+    });
+  }
 }
 QmlWeb.PixiObject = PixiObject;
 const PIXI_MODULE_NAME = "PIXI";
